5e5502e0c875f883190f52261e9f04ef768f56be
Guilherme Brendel <guilherme.brendel@owlbytes.com>
2018-08-26 22:00:43 -0300

    Dialogue system

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    Queue<string> sentences;
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        sentences = new Queue<string>();
+	}
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+       
+        nameText.text = dialogue.name;
+        sentences.Clear();
+        animator.SetBool("IsOpen", true);
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+
+        StopAllCoroutines();
+        StartCoroutine(typeSentence(sentence));
+    }
+
+    IEnumerator typeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+    void EndDialogue()
+    {
+        Debug.Log("End of conversation");
+        animator.SetBool("IsOpen", false);
+    }
+}
